821e4d40b1ad18fa8159917da87a81300fae4639
Jafeth Delgado <152584166+jaftdelgado@users.noreply.github.com>
2025-10-27 16:32:35 -0600

    feat: Refactorizacion app.js

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const app = express();
 
 app.use(express.json());
 
-//--> const mongoDBURI = 'mongodb://usuario:pass@localhost:27017/estudiantesDB';
+// const mongoDBURI = 'mongodb://usuario:pass@localhost:27017/estudiantesDB';
 const mongoDBURI = 'mongodb://localhost:27017/estudiantesDB';
 
 mongoose.connect(mongoDBURI)
-.then(() => console.log('Conectado a MongoDB'))
-.catch(err => console.error('Error al conectar a MongoDB:', err));
-
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 app.use('/api/estudiantes', require('./routes/estudiantes'));
+app.use('/api/profesores', require('./routes/profesores'));
 
 
-app.listen(3033, () => {
-    console.log('Servidor ejecutándose en http://localhost:3033');
+app.use((req, res) => {
+    res.status(404).json({ msg: 'Ruta no encontrada' });
+});
+
+const PORT = 3033;
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
